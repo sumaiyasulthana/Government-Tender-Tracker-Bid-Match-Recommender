519a9d8f4c67fa6c66acb4e7513457ef92775534
sumaiya <128169700+sumaiyasulthana@users.noreply.github.com>
2025-04-29 07:35:06 -0700

    Update scraper.py

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -20,39 +20,3 @@ def fetch_cppp_tenders():
                     'source_portal': 'CPPP'
                 })
     return tenders
-# df = pd.DataFrame(tenders)
-# Step 2: Save scraped data into SQLite
-def save_to_sqlite(tenders):
-    conn = sqlite3.connect('tender_tracker.db')
-    cursor = conn.cursor()
-
-
-    # Create table if not exists
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS tenders (
-        id INTEGER PRIMARY KEY AUTOINCREMENT, Opening_Date DateTime, Closing_Date DateTime, e_Published_Date DateTime,
-        title TEXT, Organisation Text ,source_portal Text,
-        created_at DateTime
-    )
-    ''')
-
-    # Insert data
-    cursor.execute('''
-    INSERT INTO tenders (ID, Opening_Date, Closing_Date, e_Published_Date,
-        title, Organisation,source_portal,created_at)  VALUES (?,?,?,?,?,?,?,?)
-    ''',tenders)
-
-    conn.commit()
-    conn.close()
-    print(f"✅ Saved {len(tenders)} tenders to SQLite.")
-
-# Step 3: Run the script
-def main():
-    tenders = fetch_cppp_tenders()
-    if tenders:
-        save_to_sqlite(tenders)
-    else:
-        print("No tenders found to save.")
-
-if __name__ == "__main__":
-    main()
